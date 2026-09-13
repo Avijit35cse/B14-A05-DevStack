f2402c6f8db8458993a17b4f54b84eac266747c0
Avijit35cse <ovicse35@outlook.com>
2026-09-13 19:09:33 +0600

    Banner component added

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import Banner from "./components/Banner"
+import Nav from "./components/Nav"
 
 function App() {
  
   return (
     <>
+    <Nav/>
+    <Banner/>
       
     </>
   )
---
+++ src/components/Banner.tsx
@@ -0,0 +1,34 @@
+import heroLogo from "../assets/banner-stack.png"
+const Banner = () => {
+    return (
+        <section className="flex flex-col md:flex-row lg:flex-row gap-8 lg:gap-4 container justify-between items-center mt-12 md:mt-24 mx-auto px-4 sm:px-10">
+            <div className="w-full lg:w-1/2">
+                <h2
+                    className="text-[40px] sm:text-[48px] lg:text-[60px] text-center lg:text-left font-inter font-semibold leading-[1.1] mb-4 max-w-162.5 ">
+                    Build Your Ideal<br />
+                    <span
+                        className="brand-gradient text-center">
+                        Development Stack</span>
+                </h2>
+                <p
+                    className="text-[18px] text-[#475569] text-center lg:text-left font-normal mb-8">
+                    Explore frontend, backend, database, and tooling options,
+                    compare them side by side, and put together the stack that fits your
+                    next project.
+                </p>
+                <div
+                    className="flex gap-2 justify-center lg:justify-start">
+                    <button
+                        className="btn bg-linear-to-r from-[#f97316] to-[#ec4899] text-[#ffffff] text-[14px] font-semibold rounded-xl">
+                        Explore Technologies</button>
+                    <button className="btn rounded-xl border border-[#E5E7EB] text-[14px] text-[#374151] font-normal">Learn More</button>
+                </div>
+            </div>
+
+            <img className="w-70 sm:w-87.5 lg:w-125 " src={heroLogo} alt="heroLogo" />
+
+        </section>
+    );
+};
+
+export default Banner;
